fbea99ae1ccde1dd51bdce3dd6b2ad72f7722ac7
Godson <excanny@yahoo.com>
2021-11-18 01:43:17 +0100

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,57 @@
+﻿using EmployeeADONet.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeADONet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeDataAccessLayer _objemployee;
+        public EmployeeController(EmployeeDataAccessLayer objemployee)
+        {
+            _objemployee = objemployee;
+        }
+
+        [HttpGet]
+        [Route("api/Employee/Index")]
+        public IEnumerable<Employee> Index()
+        {
+            return _objemployee.GetAllEmployees();
+        }
+
+        [HttpPost]
+        [Route("api/Employee/Create")]
+        public int Create([FromBody] Employee employee)
+        {
+            return _objemployee.AddEmployee(employee);
+        }
+
+        [HttpGet]
+        [Route("api/Employee/Details/{id}")]
+        public Employee Details(int id)
+        {
+            return _objemployee.GetEmployee(id);
+        }
+
+        [HttpPut]
+        [Route("api/Employee/Edit")]
+        public int Edit([FromBody] Employee employee)
+        {
+            return _objemployee.UpdateEmployee(employee);
+        }
+
+        [HttpDelete]
+        [Route("api/Employee/Delete/{id}")]
+        public int Delete(int id)
+        {
+            return _objemployee.DeleteEmployee(id);
+        }
+
+    }
+}
---
+++ Models/EmployeeDataAccessLayer.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeADONet.Models
+{
+    public class EmployeeDataAccessLayer
+    {
+        string connectionString = "Put Your Connection string here";
+
+        //To View all employees details
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            try
+            {
+                List<Employee> employeeslist = new List<Employee>();
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("spGetAllEmployees", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Employee employee = new Employee();
+
+                        employee.ID = Convert.ToInt32(reader["EmployeeID"]);
+                        employee.Name = reader["Name"].ToString();
+                        employee.Gender = reader["Gender"].ToString();
+                        employee.Department = reader["Department"].ToString();
+                        employee.City = reader["City"].ToString();
+
+                        employeeslist.Add(employee);
+                    }
+
+                    con.Close();
+
+                    return employeeslist;
+
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            try
+            {
+                Employee employee = new Employee();
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    string sqlQuery = "SELECT * FROM tblEmployee WHERE EmployeeID= " + id;
+                    SqlCommand cmd = new SqlCommand(sqlQuery, con);
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        employee.ID = Convert.ToInt32(rdr["EmployeeID"]);
+                        employee.Name = rdr["Name"].ToString();
+                        employee.Gender = rdr["Gender"].ToString();
+                        employee.Department = rdr["Department"].ToString();
+                        employee.City = rdr["City"].ToString();
+                    }
+                }
+                return employee;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public int AddEmployee(Employee employee)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("spGetAllEmployees", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@EmpId", employee.ID);
+                    cmd.Parameters.AddWithValue("@Name", employee.Name);
+                    cmd.Parameters.AddWithValue("@Gender", employee.Gender);
+                    cmd.Parameters.AddWithValue("@Department", employee.Department);
+                    cmd.Parameters.AddWithValue("@City", employee.City);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    return 1;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public int UpdateEmployee(Employee employee)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("spUpdateEmployee", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@EmpId", employee.ID);
+                    cmd.Parameters.AddWithValue("@Name", employee.Name);
+                    cmd.Parameters.AddWithValue("@Gender", employee.Gender);
+                    cmd.Parameters.AddWithValue("@Department", employee.Department);
+                    cmd.Parameters.AddWithValue("@City", employee.City);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public int DeleteEmployee(int id)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("spDeleteEmployee", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@EmpId", id);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+    }
+}
