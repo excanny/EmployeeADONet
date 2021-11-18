28bfd8561f503697f34595388b91190e96c0d241
Godson <excanny@yahoo.com>
2021-11-18 02:06:23 +0100

    fixes

--- Models/EmployeeDataAccessLayer.cs
+++ Models/EmployeeDataAccessLayer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -9,7 +10,13 @@ namespace EmployeeADONet.Models
 {
     public class EmployeeDataAccessLayer
     {
-        string connectionString = "Put Your Connection string here";
+        public IConfiguration _configuration { get; }
+        public EmployeeDataAccessLayer(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        string connectionString = _configuration["ConnectionStrings:DefaultConnection"];
 
         //To View all employees details
         public IEnumerable<Employee> GetAllEmployees()
@@ -20,28 +27,32 @@ namespace EmployeeADONet.Models
 
                 using (SqlConnection con = new SqlConnection(connectionString))
                 {
-                    SqlCommand cmd = new SqlCommand("spGetAllEmployees", con);
-                    cmd.CommandType = CommandType.StoredProcedure;
 
-                    con.Open();
-                    SqlDataReader reader = cmd.ExecuteReader();
-
-                    while (reader.Read())
+                    using (SqlCommand cmd = new SqlCommand("spGetAllEmployees", con))
                     {
-                        Employee employee = new Employee();
+                        cmd.CommandType = CommandType.StoredProcedure;
 
-                        employee.ID = Convert.ToInt32(reader["EmployeeID"]);
-                        employee.Name = reader["Name"].ToString();
-                        employee.Gender = reader["Gender"].ToString();
-                        employee.Department = reader["Department"].ToString();
-                        employee.City = reader["City"].ToString();
+                        con.Open();
+                        SqlDataReader reader = cmd.ExecuteReader();
 
-                        employeeslist.Add(employee);
+                        while (reader.Read())
+                        {
+                            Employee employee = new Employee();
+
+                            employee.ID = Convert.ToInt32(reader["EmployeeID"]);
+                            employee.Name = reader["Name"].ToString();
+                            employee.Gender = reader["Gender"].ToString();
+                            employee.Department = reader["Department"].ToString();
+                            employee.City = reader["City"].ToString();
+
+                            employeeslist.Add(employee);
+                        }
+
+                        con.Close();
+
+                        return employeeslist;
+
                     }
-
-                    con.Close();
-
-                    return employeeslist;
 
                 }
                
@@ -60,16 +71,18 @@ namespace EmployeeADONet.Models
                 using (SqlConnection con = new SqlConnection(connectionString))
                 {
                     string sqlQuery = "SELECT * FROM tblEmployee WHERE EmployeeID= " + id;
-                    SqlCommand cmd = new SqlCommand(sqlQuery, con);
-                    con.Open();
-                    SqlDataReader rdr = cmd.ExecuteReader();
-                    while (rdr.Read())
+                    using (SqlCommand cmd = new SqlCommand(sqlQuery, con))
                     {
-                        employee.ID = Convert.ToInt32(rdr["EmployeeID"]);
-                        employee.Name = rdr["Name"].ToString();
-                        employee.Gender = rdr["Gender"].ToString();
-                        employee.Department = rdr["Department"].ToString();
-                        employee.City = rdr["City"].ToString();
+                        con.Open();
+                        SqlDataReader rdr = cmd.ExecuteReader();
+                        while (rdr.Read())
+                        {
+                            employee.ID = Convert.ToInt32(rdr["EmployeeID"]);
+                            employee.Name = rdr["Name"].ToString();
+                            employee.Gender = rdr["Gender"].ToString();
+                            employee.Department = rdr["Department"].ToString();
+                            employee.City = rdr["City"].ToString();
+                        }
                     }
                 }
                 return employee;
@@ -86,20 +99,56 @@ namespace EmployeeADONet.Models
             {
                 using (SqlConnection con = new SqlConnection(connectionString))
                 {
-                    SqlCommand cmd = new SqlCommand("spGetAllEmployees", con);
-                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (SqlCommand cmd = new SqlCommand("spGetAllEmployees", con))
+                    {
 
-                    cmd.Parameters.AddWithValue("@EmpId", employee.ID);
-                    cmd.Parameters.AddWithValue("@Name", employee.Name);
-                    cmd.Parameters.AddWithValue("@Gender", employee.Gender);
-                    cmd.Parameters.AddWithValue("@Department", employee.Department);
-                    cmd.Parameters.AddWithValue("@City", employee.City);
+                    
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+
+                    //adding parameters
+
+                    SqlParameter parameter = new SqlParameter
+                    {
+                        ParameterName = "@Name",
+                        Value = employee.Name,
+                        SqlDbType = SqlDbType.VarChar,
+                        Size = 50
+                    };
+                    cmd.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@Gender",
+                        Value = employee.Gender,
+                        SqlDbType = SqlDbType.VarChar
+                    };
+                    cmd.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@Department",
+                        Value = employee.Department,
+                        SqlDbType = SqlDbType.VarChar
+                    };
+                    cmd.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@City",
+                        SqlDbType = SqlDbType.VarChar,
+                        Size = 50,
+                        Direction = ParameterDirection.Output
+                    };
+                    cmd.Parameters.Add(parameter);
+
 
                     con.Open();
                     cmd.ExecuteNonQuery();
                     con.Close();
 
                     return 1;
+                    }
                 }
             }
             catch (Exception ex)
@@ -115,16 +164,18 @@ namespace EmployeeADONet.Models
             {
                 using (SqlConnection con = new SqlConnection(connectionString))
                 {
-                    SqlCommand cmd = new SqlCommand("spUpdateEmployee", con);
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@EmpId", employee.ID);
-                    cmd.Parameters.AddWithValue("@Name", employee.Name);
-                    cmd.Parameters.AddWithValue("@Gender", employee.Gender);
-                    cmd.Parameters.AddWithValue("@Department", employee.Department);
-                    cmd.Parameters.AddWithValue("@City", employee.City);
-                    con.Open();
-                    cmd.ExecuteNonQuery();
-                    con.Close();
+                    using (SqlCommand cmd = new SqlCommand("spUpdateEmployee", con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@EmpId", employee.ID);
+                        cmd.Parameters.AddWithValue("@Name", employee.Name);
+                        cmd.Parameters.AddWithValue("@Gender", employee.Gender);
+                        cmd.Parameters.AddWithValue("@Department", employee.Department);
+                        cmd.Parameters.AddWithValue("@City", employee.City);
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
                 }
                 return 1;
             }
@@ -140,13 +191,15 @@ namespace EmployeeADONet.Models
             {
                 using (SqlConnection con = new SqlConnection(connectionString))
                 {
-                    SqlCommand cmd = new SqlCommand("spDeleteEmployee", con);
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@EmpId", id);
+                    using (SqlCommand cmd = new SqlCommand("spDeleteEmployee", con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@EmpId", id);
 
-                    con.Open();
-                    cmd.ExecuteNonQuery();
-                    con.Close();
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
                 }
                 return 1;
             }
